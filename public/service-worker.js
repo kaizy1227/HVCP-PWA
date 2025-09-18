0c8bd3f2df097fb7efd7035d7c28cb74a9a1fa17
kaizy1227 <kaizy1227@gmail.com>
2025-09-18 12:39:44 +0700

    PWA

---
+++ public/service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "hvcp-cache-v1";
+const urlsToCache = ["/", "/index.html", "/manifest.json"];
+
+// Cài đặt service worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Kích hoạt service worker
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch offline caching
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
